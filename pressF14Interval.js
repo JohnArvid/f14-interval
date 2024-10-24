f2904dc6fbca762bc87fdd3b29f50057f5b885db
JohnArvid <arvid.lindstrom@live.se>
2024-10-24 11:55:43 +0200

    remove log message

--- pressF14Interval.js
+++ pressF14Interval.js
@@ -5,7 +5,7 @@ const { keyboard, Key } = require('@nut-tree-fork/nut-js');
 async function pressF14() {
   await keyboard.pressKey(Key.F14);
   await keyboard.releaseKey(Key.F14);
-  console.log('20 seconds passed');
+  // console.log('20 seconds passed');
 }
 
 // Anropa funktionen var 10 sekund (10000 millisekunder)
