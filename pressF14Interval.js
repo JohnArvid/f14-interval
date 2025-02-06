328f679d3a16938ef16b0547aae64c9326aa9c74
JohnArvid <arvid.lindstrom@live.se>
2025-02-06 08:45:29 +0100

    Implementera stopptid

--- pressF14Interval.js
+++ pressF14Interval.js
@@ -1,11 +1,24 @@
 // Importera nut.js
 const { keyboard, Key } = require('@nut-tree-fork/nut-js');
 
+let stopTime = '16:43:00';
+
 // Simulera tryck på F14
 async function pressF14() {
   await keyboard.pressKey(Key.F14);
   await keyboard.releaseKey(Key.F14);
+  // console.log('Running');
+  let now = new Date().toLocaleTimeString();
+  if (now > stopTime) {
+    // console.log('Stopped');
+    stopF14();
+  }
+}
+
+// Funktion för att stoppa exekvering
+function stopF14() {
+  clearInterval(intervalID);
 }
 
 // Anropa funktionen var 20 sekund (20000 millisekunder)
-setInterval(pressF14, 20000);
+let intervalID = setInterval(pressF14, 20000);
